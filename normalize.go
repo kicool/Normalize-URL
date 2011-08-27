29efa866dd83fb7f2f00f0bc3fd46fff85447a6d
Apphacker <apphacker@gmail.com>
2011-08-27 00:15:20 -0700

    Progress on tests.

--- normalize.go
+++ normalize.go
@@ -5,7 +5,8 @@ import (
 )
 
 func Normalize(rawurl string) (url string, err os.Error) {
-  return "Not yet implemented.", *(NewNormalizeError("Not yet implmented"))
+  url = rawurl
+  return url, nil
 }
 
 func NewNormalizeError(description string) *NormalizeError {
@@ -21,3 +22,39 @@ type NormalizeError struct {
 func (err NormalizeError) String() string {
   return err.err
 }
+
+//Character values 0-31 need to be escaped in query strings:
+var controlCharEnd int = 31
+var reservedChars = [...]int{
+  36,  //$
+  38,  //&
+  43,  //+
+  44,  //,
+  47,  ///
+  58,  //:
+  59,  //;
+  61,  //=
+  63,  //?
+  64,  //@
+  127, //.
+}
+var unsafeChars = [...]int{
+  32,  //space
+  34,  //"
+  35,  //#
+  37,  //%
+  60,  //<
+  62,  //
+  91,  //[
+  92,  //\
+  93,  //]
+  94,  //^
+  96,  //`
+  123, //{
+  124, //|
+  125, //}
+  126, //~
+}
+//Character values 128-255 need to be escaped.
+var nonASCIImin int = 128
+var nonASCIImax int = 255
--- normalize_test.go
+++ normalize_test.go
@@ -2,10 +2,126 @@ package normalize
 
 import (
   "testing"
+  "strconv"
+  "strings"
+  "sort"
   "fmt"
 )
 
+var rawURLs = [...]string{
+  "HtTp://spHela.com",
+  "HTTps://www.EXAMPLE.COM/%2d%aD/MOO#smoo",
+  "HTTps://www.EXAMPLE.COM/%2d%aD/?MO=O smoo",
+  "HTTps://www.EXAMPLE.COM/%2d%aD/MOO ",
+  "http://apphacker.com/moo/../doo/./baz/",
+  "http://apphacker.com/moo//doo//baz/",
+  "http://www.apphacker.com?",
+  "http://www.apphacker.com?boo=fuzz",
+  "http://apphacker.com/?",
+  "http://apphacker.com/?%foo=bar",
+}
+
+var normalizedURLs = [...]string{
+  "http://sphela.com/",
+  "https://www.example.com/~%AD/MOO#smoo",
+  "https://www.example.com/~%AD/?MO=O%20smoo",
+  "https://www.example.com/~%AD/MOO",
+  "http://apphacker.com/moo/doo/baz/",
+  "http://apphacker.com/moo/doo/baz/",
+  "http://www.apphacker.com/",
+  "http://www.apphacker.com/?boo=fuzz",
+  "http://apphacker.com/",
+  "http://apphacker.com/?%25foo=bar",
+}
+
 func TestNormalize(t *testing.T) {
-  fmt.Println("Not yet testing anything.")
-  t.Log("Not yet testing anything.")
+  for i, checkURL := range rawURLs {
+    if receivedURL, err := Normalize(checkURL); err == nil {
+      if receivedURL != normalizedURLs[i] {
+        t.Error("Received URL not normalized", receivedURL, normalizedURLs[i])
+      }
+    } else {
+      t.Error("Error while normalizing ", err)
+    }
+  }
+}
+
+func testChar(t *testing.T, val int) {
+  var (
+    char byte
+    hex  string
+  )
+  testURL := "http://google.com?moo=doo"
+  char = byte(val)
+  hex = strings.ToUpper(strconv.Itob(val, 16))
+  if len(hex) == 1 {
+    hex = "0" + hex
+  }
+  checkURL := testURL + string(char)
+  normalizedURL := testURL
+  if char != ' ' {
+    //Trailing whitespaces are removed.
+    normalizedURL = normalizedURL + "%" + hex
+  }
+  if receivedURL, err := Normalize(checkURL); err == nil {
+    if receivedURL != normalizedURL {
+      t.Error("Character not escaped right.", checkURL,
+        normalizedURL, receivedURL)
+    }
+  } else {
+    t.Error("Error while normalizing ", err)
+  }
+}
+
+func TestControlChars(t *testing.T) {
+  for i := 0; i < controlCharEnd; i++ {
+    testChar(t, i)
+  }
+}
+
+func TestReservedChars(t *testing.T) {
+  for _, val := range reservedChars {
+    testChar(t, val)
+  }
+}
+
+func TestSomeUnsafeChars(t *testing.T) {
+  for _, val := range reservedChars {
+    if val != 35 || val != 37 {
+      testChar(t, val)
+    }
+  }
+}
+
+func TestNonASCIIChars(t *testing.T) {
+  for i := nonASCIImin; i <= nonASCIImax; i++ {
+    testChar(t, i)
+  }
+}
+
+func TestUnescapeChars(t *testing.T) {
+  searchSlice := make(sort.IntSlice, len(reservedChars)+len(unsafeChars))
+  copy(searchSlice, reservedChars[:])
+  copy(searchSlice, unsafeChars[:])
+  searchSlice.Sort()
+  for i := 0; i < 256; i++ {
+    index := searchSlice.Search(i)
+    fmt.Println("Index:", index, "searchSlice[index]:", searchSlice[index],
+      "len(searchSlice):", len(searchSlice), "i:", i)
+    switch {
+    default:
+      //Do something
+      fmt.Println("Searching and testing against", i)
+    case i <= controlCharEnd:
+      fmt.Println("Less than controlCharEnd", i, controlCharEnd)
+      continue
+    case i >= nonASCIImin && i <= nonASCIImax:
+      fmt.Println("In non-ASCII range", i, nonASCIImin, nonASCIImax)
+      continue
+    case index < len(searchSlice):
+      fmt.Println("Found in searchSlice i:", i, "index:", index,
+        "searchSlice[index]:", searchSlice[index])
+      continue
+    }
+  }
 }
